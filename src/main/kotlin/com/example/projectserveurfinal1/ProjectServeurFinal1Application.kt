927e3b681655e3218458184f0fa72f37b7168c9c
clepshydre <clement.ifrah@gmail.com>
2022-10-14 11:09:04 +0200

    first commit

---
+++ src/main/kotlin/com/example/projectserveurfinal1/ProjectServeurFinal1Application.kt
@@ -0,0 +1,11 @@
+package com.example.projectserveurfinal1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectServeurFinal1Application
+
+fun main(args: Array<String>) {
+    runApplication<ProjectServeurFinal1Application>(*args)
+}
---
+++ src/main/kotlin/com/example/projectserveurfinal1/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.example.projectserveurfinal1
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(ProjectServeurFinal1Application::class.java)
+    }
+
+}
